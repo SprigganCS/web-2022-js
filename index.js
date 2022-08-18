c8d94da61724c110b79f97285f176c91faeafc12
SprigganCS <andrenmed@hotmail.com>
2022-08-18 16:51:49 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('loginbtn');
+
+btn.addEventListener('click', function () {
+    const user = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
+    if (user == 'admin' && pass == 'admin') {
+        window.location.href = 'guests.html';
+    }
+});
