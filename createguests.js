e343395ebc95f85cfb5b226c2730900df391d2bd
SprigganCS <andrenmed@hotmail.com>
2022-08-23 01:58:22 -0300

    backend integrado

---
+++ createguests.js
@@ -0,0 +1,11 @@
+
+document.getElementById('savebtn').addEventListener('click', function () {
+    let firstname = document.getElementById('firstname').value;
+    let lastname = document.getElementById('lastname').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+    
+});
+
+    
+    
--- guests.js
+++ guests.js
@@ -7,28 +7,49 @@ function httpGet(url) {
 
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    var obj = JSON.parse(httpGet("https://cors-anywhere.herokuapp.com/https://hostel-app-back-end-api.herokuapp.com/customers"));
+document.addEventListener('DOMContentLoaded', function (){
+    var obj = JSON.parse(httpGet("https://hostel-app-back-end-api.herokuapp.com/customers"));
 
     var table = document.getElementById("gueststable");
+    let size = Object.keys(obj).length;
+    console.log(obj);
+    
+    for(let i=0; i<size; i++){
 
-    var size = Object.keys(obj).length;
-    console.log(size);
-
-    for (i = 0; i < size; i++) {
         var row = table.insertRow(1);
         var cell = row.insertCell(0);
-        cell.innerHTML = guests.email;
+        cell.innerHTML = obj[size-i-1]["emailAddress"];
         cell = row.insertCell(0);
-        cell.innerHTML = guests.Lname;
+        cell.innerHTML = obj[size-i-1]["lastName"];
         cell = row.insertCell(0);
-        cell.innerHTML = guests.Fname;
+        cell.innerHTML = obj[size-i-1]["firstName"];
         cell = row.insertCell(0);
-        cell.innerHTML = guests.num;
+        cell.innerHTML = obj[size-i-1]["phoneNumber"];
         cell = row.insertCell(0);
-        cell.innerHTML = guests.id;
+        cell.innerHTML = obj[size-i-1]["id"];
     }
 });
+
+
+document.getElementById('savebtn').addEventListener('click', function () {
+    window.location.href="guests.html";
+    let firstname = document.getElementById('firstname').value;
+    let lastname = document.getElementById('lastname').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
+    
+    var row = table.insertRow(1);
+    var cell = row.insertCell(0);
+    cell.innerHTML = email;
+    cell = row.insertCell(0);
+    cell.innerHTML = lastname;
+    cell = row.insertCell(0);
+    cell.innerHTML = firstname;
+    cell = row.insertCell(0);
+    cell.innerHTML = phone;
+    cell = row.insertCell(0);
+    cell.innerHTML = size+1;
+}); 
 
 
 
