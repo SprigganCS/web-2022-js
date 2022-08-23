67fa2ba10f615d031f0540cc0e1b59837b0b52a2
SprigganCS <andrenmed@hotmail.com>
2022-08-23 01:00:24 -0300

    connection ok

--- controller.js
+++
@@ -1,6 +0,0 @@
-function httpGet(url) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url, false);
-    xmlHttp.send(null);
-    return xmlHttp.responseText;
-}
--- guests.js
+++ guests.js
@@ -1,22 +1,34 @@
-let Fname = "joao";
-let Lname = "silva";
-let num = "+55 17 666666666";
-let email = "dsadsadsa.com";
-let id = 2;
-
-
-var table = document.getElementById("gueststable");
-
-for(i=0; i<guests.lenght; i++){
-    var row = table.insertRow(1);
-    var cell = row.insertCell(0);
-    cell.innerHTML = guests.email;
-    cell = row.insertCell(0);
-    cell.innerHTML = guests.Lname;
-    cell = row.insertCell(0);
-    cell.innerHTML = guests.Fname;
-    cell = row.insertCell(0);
-    cell.innerHTML = guests.num;
-    cell = row.insertCell(0);
-    cell.innerHTML = guests.id;
+function httpGet(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
 }
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var obj = JSON.parse(httpGet("https://cors-anywhere.herokuapp.com/https://hostel-app-back-end-api.herokuapp.com/customers"));
+
+    var table = document.getElementById("gueststable");
+
+    var size = Object.keys(obj).length;
+    console.log(size);
+
+    for (i = 0; i < size; i++) {
+        var row = table.insertRow(1);
+        var cell = row.insertCell(0);
+        cell.innerHTML = guests.email;
+        cell = row.insertCell(0);
+        cell.innerHTML = guests.Lname;
+        cell = row.insertCell(0);
+        cell.innerHTML = guests.Fname;
+        cell = row.insertCell(0);
+        cell.innerHTML = guests.num;
+        cell = row.insertCell(0);
+        cell.innerHTML = guests.id;
+    }
+});
+
+
+
